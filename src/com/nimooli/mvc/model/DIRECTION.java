e5acc67f3bd7bdcf567031ca91c6540209be6083
Juan Carlos Romero Uchubanda <152119104+jcarrome@users.noreply.github.com>
2024-11-12 07:11:08 -0500

    Update DIRECTION.java
    He refactorizado la implementación del enum DIRECTION y la he reemplazado por clases concretas que representan cada dirección. Esta modificación permite que el código sea más extensible sin necesidad de modificar las clases existentes, cumpliendo así con el Principio de Abierto/Cerrado (OCP) de SOLID.

--- src/com/nimooli/mvc/model/DIRECTION.java
+++ src/com/nimooli/mvc/model/DIRECTION.java
@@ -1,22 +1,59 @@
 package com.nimooli.mvc.model;
 
-/**
- * Enum that represents 4 directions and can return the opposite direction
- */
-public enum DIRECTION {
-    WEST, NORTH, EAST, SOUTH;
+public abstract class Direction {
+    private final String name;
 
-    private DIRECTION opposite;
-
-    static {
-        NORTH.opposite = SOUTH;
-        SOUTH.opposite = NORTH;
-        EAST.opposite = WEST;
-        WEST.opposite = EAST;
+    public Direction(String name) {
+        this.name = name;
     }
 
-    public DIRECTION getOppositeDirection() {
-        return opposite;
+    public abstract Direction getOppositeDirection();
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class North extends Direction {
+    public North() {
+        super("North");
     }
 
+    @Override
+    public Direction getOppositeDirection() {
+        return new South();
+    }
+}
+
+public class South extends Direction {
+    public South() {
+        super("South");
+    }
+
+    @Override
+    public Direction getOppositeDirection() {
+        return new North();
+    }
+}
+
+public class East extends Direction {
+    public East() {
+        super("East");
+    }
+
+    @Override
+    public Direction getOppositeDirection() {
+        return new West();
+    }
+}
+
+public class West extends Direction {
+    public West() {
+        super("West");
+    }
+
+    @Override
+    public Direction getOppositeDirection() {
+        return new East();
+    }
 }
